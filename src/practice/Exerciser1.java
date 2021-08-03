f31f974985137e6a5e35a8f91eb664ac6ecdc215
LAPTOP-7P15NC54\hung <nqhung141196@gmail.com>
2021-08-03 18:21:57 +0700

    Exercise 1

--- src/practice/Exerciser1.java
+++ src/practice/Exerciser1.java
@@ -1,8 +1,21 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Exerciser1 {
     public static void main(String[] args) {
-        System.out.println("ABC");
-        
+        System.out.println("Enter String");
+        Scanner input = new Scanner(System.in);
+        String x = input.nextLine();
+        char search = 'a';
+        int count = 0;
+
+        for (int i=0; i<x.length();i++){
+            if (x.charAt(i)==search){
+                count++;
+            }
+        }
+        System.out.println("Number of a is:"+count);
     }
 }
+
